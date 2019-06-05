76643570dfc22b8a6797068c6664e260999cbcf5
kevinJ <jaipersaudk@gmail.com>
2019-06-05 13:23:32 -0400

    Question 4

--- HW_Chapter1.cxx
+++ HW_Chapter1.cxx
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 
@@ -17,9 +18,39 @@ using namespace std;
   }
 
 // Problem 2 - Write	an	algorithm	that	finds	the	m	smallest	numbers	in	a	list	of	n	numbers
-// first sort the array and then print the first m values
+// First sort the array and then print the first m values
 
   void minimums(int arr[], int m, int n)
   {
-    sort 
+    sort(arr,arr+n); //sorts the array in ascending order
+    for (int i = 0; i<m; i++)
+    {
+      cout << arr[i] << endl;
+    }
   }
+
+/* Problem 4: Write	an	Insertion	Sort	algorithm	(Insertion	Sort	is	discussed	in	Section	7.2) that	uses
+* Binary	Search	to	find	the	position	where	the	next	insertion	should take	place */
+// doing a binary insertion sort - searching the sorted side of the array to find where to insert the next element
+
+    int binarySearch(int arr[], int start, int end, int target)
+    {
+      if (end >= start)
+      {
+        int mid = (start+(end/2)); //the middle term of the array
+
+        if (arr[mid] == target) //if the middle term is what your are looking for
+          return mid;
+
+        else if (target < arr[mid]) //search the left side of the array
+          binarySearch(arr, start, mid-1,target);
+
+        else if (target > arr[mid]) //if target is bigger than the middle term, search the right side of the array
+          binarySearch(arr, mid+1, end, target);
+
+        return -1; //if not found
+      }
+
+      // insertion sort algorithm
+
+    }
