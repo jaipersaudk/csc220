5b95bb29ea4ac84002f0853ee9fb4f5772ee8314
kevinJ <jaipersaudk@gmail.com>
2019-06-05 10:08:09 -0400

    Finished Q1

--- HW_Chapter1.cxx
+++ HW_Chapter1.cxx
@@ -1,8 +1,25 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
-  // Problem 1
-  
-}
+
+// Problem 1 - Write	an	algorithm	that	finds	the	largest	number	in	a	list	(an	array)	of	n numbers.
+// Sequential search algorithm
+
+  int maximum(int n, int arr[])
+  {
+  int max = arr[0]; //max element
+  for (int i = 0; i < n; i++)
+  {
+    if (arr[i] > max)
+      max = arr[i];
+  }
+  return max;
+  }
+
+// Problem 2 - Write	an	algorithm	that	finds	the	m	smallest	numbers	in	a	list	of	n	numbers
+// first sort the array and then print the first m values
+
+  void minimums(int arr[], int m, int n)
+  {
+    sort 
+  }
