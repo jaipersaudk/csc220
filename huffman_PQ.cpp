d19a86f6f8355c879f205666075f1160c43a49cf
kevinJ <jaipersaudk@gmail.com>
2019-07-26 17:14:07 -0400

    Done

--- huffman_PQ.cpp
+++ huffman_PQ.cpp
@@ -8,12 +8,12 @@ using namespace std;
 struct huffTreeNode
 {
   // global variables
-  int freq; // frequency of given data
+  int freq; // total frequency at given node
   unsigned char data;
   const huffTreeNode *left_child; // left and right child for the heap
   const huffTreeNode *right_child;
 
-  // constructor
+
   huffTreeNode(unsigned char d = 0, int i = -1)
   {
     // inital values for global variables
@@ -25,7 +25,7 @@ struct huffTreeNode
 
   huffTreeNode(const huffTreeNode* c1, const huffTreeNode* c2)
   {
-    data = 0;
+    //data = 0;
     freq = c1->freq + c2->freq; // the weight of node is the sum of the left and right child
     left_child = c1;
     right_child = c2;
@@ -38,22 +38,22 @@ struct huffTreeNode
   }
 
 
-  void traversing (string codeValue="") const //each data starts with blank codevalue
+  void traversing (string binary_Value="") const //each data starts with blank binary_Value
   {
     if (left_child) //if there is a left child
     {
       // traverse the left subtree first
-      left_child->traversing(codeValue+'0'); //left subtree gets a codevalue of 0
-      right_child->traversing(codeValue+'1'); //right subtrees get a codevalue of 1
+      left_child->traversing(binary_Value+'0'); //left subtree gets a binary_Value of 0
+      right_child->traversing(binary_Value+'1'); //right subtrees get a binary_Value of 1
     }
 
 
-    // since heap property must be maintained, if there is no left child, then print the data, the frequency and the codevalue
+    // since heap property must be maintained, if there is no left child, then print the data, the frequency and the binary_Value
     else
     {
       cout << "    " << data;
       cout << "\t\t    " << freq;
-      cout << "\t\t " << codeValue;
+      cout << "\t\t " << binary_Value;
       cout << "\n";
     }
   }
@@ -69,11 +69,11 @@ void Count_characters(int* counter)
   //inputting the raven poem
   ifstream myinputFile("Raven_poem.txt");
 
-  if(!myinputFile)
-  {
-    cout << "Can't open input file" << endl;
-    throw "abort";
-  }
+  // if(!myinputFile)
+  // {
+  //   cout << "Can't open input file" << endl;
+  //   throw "abort";
+  // }
 
   myinputFile.setf(ios::skipws); // needed to skip the whitespace of the file
 
@@ -91,32 +91,32 @@ void Count_characters(int* counter)
 
 int main()
 {
-  // declaring an array for the characters and using the array in Count_characters function
+  // declaring an empty array for the characters and using the array in the Count_characters function
   int counter[256] = {};
   Count_characters(counter);
 
-  // implementing priority queue
-  priority_queue < huffTreeNode > myQ;
+  // implementing priority queue called pQ with data structure huffTreeNode
+  priority_queue<huffTreeNode> pQ;
   for (int i = 0; i < 256; i++)
     if(counter[i])
-      myQ.push(huffTreeNode(i,counter[i]));
+      pQ.push(huffTreeNode(i,counter[i]));
 
-  // pushing values from priority onto the min heap
-  while(myQ.size() > 1)
+  // implementing a heap for the huffman tree
+  while(pQ.size() > 1)
   {
     huffTreeNode* left_child = new
-    huffTreeNode(myQ.top());
-    myQ.pop();
+    huffTreeNode(pQ.top());
+    pQ.pop();
     huffTreeNode* right_child = new
-    huffTreeNode(myQ.top());
-    myQ.pop();
-    myQ.push(huffTreeNode(left_child, right_child));
+    huffTreeNode(pQ.top());
+    pQ.pop();
+    pQ.push(huffTreeNode(left_child, right_child));
   }
 
-  // output
-  cout << "CHARACTER \t FREQUENCY \t HUFFMANCODE" << endl;
-  cout << "--------- \t --------- \t -----------" << endl;
-  myQ.top().traversing();
+  // printing the output values
+  /* cout << "CHARACTER \t FREQUENCY \t HUFFMANCODE" << endl;
+   cout << "--------- \t --------- \t -----------" << endl; */
+  pQ.top().traversing();
   getchar();
   return 0;
 }
